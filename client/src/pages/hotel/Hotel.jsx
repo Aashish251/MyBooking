166f5a72479b35324616196933a66ed0a52f99ad
Aashish <aashishravidas251@gmail.com>
2022-08-17 00:28:29 +0530

    admin and login

--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -10,9 +10,13 @@ import {
   faCircleXmark,
   faLocationDot,
 } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useContext, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
+import { SearchContext } from "../../context/SearchContext";
+import { AuthContext } from "../../context/AuthContext";
+import Reserve from "../../components/reserve/Reserve";
+
 
 const Hotel = () => {
 
@@ -20,10 +24,24 @@ const Hotel = () => {
   const id = location.pathname.split("/")[2];
   const [slideNumber, setSlideNumber] = useState(0);
   const [open, setOpen] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
 
-  const { data , loading , error } = useFetch(`/hotels/find/${id}`)
+  const { data , loading , error } = useFetch(`/hotels/find/${id}`);
+
+  const { user} = useContext(AuthContext)
+  const navigate = useNavigate();
+   
+  const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24;
+  function dayDifference(date1, date2) {
+    const timeDiff = Math.abs(date2.getTime() - date1.getTime());
+    const diffDays = Math.ceil(timeDiff / MILLISECONDS_PER_DAY);
+    return diffDays;
+  }
 
   
+  const {dates, options} = useContext(SearchContext);
+  const days = dayDifference(dates[0].endDate, dates[0].startDate);
+
 
   const handleOpen = (i) => {
     setSlideNumber(i);
@@ -41,6 +59,16 @@ const Hotel = () => {
 
     setSlideNumber(newSlideNumber)
   };
+
+
+  const handleClick = () => {
+    if (user) {
+      setOpenModal(true);
+    } else {
+      navigate("/login");
+    }
+  };
+
 
   return (
     <div>
@@ -110,9 +138,10 @@ const Hotel = () => {
                 excellent location score of 9.8!
               </span>
               <h2>
-                <b>₹129999</b> (9 nights)
+              <b>₹{days * data.cheapestPrice * options.room}</b> ({days}{" "}
+                  nights)
               </h2>
-              <button>Reserve or Book Now!</button>
+              <button onClick={handleClick}>Reserve or Book Now!</button>
             </div>
           </div>
         </div>
@@ -120,6 +149,8 @@ const Hotel = () => {
         <Footer />
       </div> 
       )}
+
+{openModal && <Reserve setOpen={setOpenModal} hotelId={id}/>}
     </div>
   );
 };
